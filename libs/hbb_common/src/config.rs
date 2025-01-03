1ba60002bc02318dfec822cdf78866622a10d630
8080713 <121084065+8080713@users.noreply.github.com>
2025-01-03 21:29:41 +0800

    config.rs
    修改链接地址

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.co1m"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.78.3.142"];
+pub const PUBLIC_RS_PUB_KEY: &str = "XooJTbkT6qiqRjXJelrh79mRLHhZPSpaqXpI6bYbg5E=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
